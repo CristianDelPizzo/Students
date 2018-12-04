172bcb3f01d647fce0624cbf748cd7a4c9bd3e2b
Delpy991 <45163293+Delpy991@users.noreply.github.com>
2018-12-04 21:59:09 +0100

    Add files via upload

---
+++ StudentsSolution/StudentsSolution/StudentsSolution/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace StudentsSolution
+{
+    class Program
+    {
+        public static List<Student> StudentsList = new List<Student>();
+
+        static void Main(string[] args)
+        {
+            Presentation();
+            var reader = new StreamReader(@"C:\Users\crisy\source\repos\Delpy991\Corso\StudentsSolution\StudentsSolution\StudentsSolution\Students.csv");
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    StudentsList.Add(new Student(Convert.ToInt32(values[0]), values[1], values[2], Convert.ToInt32(values[3]), values[4], Convert.ToDouble(values[5])));
+                
+                }
+            }
+
+            
+        }
+
+        private static void Presentation()
+        {
+            Console.WriteLine("Ecco i comandi che puoi eseguire:\n" +
+            "Comando l: Lista dei studenti\n" +
+            "Comando f: Ricerca per ID Studente\n" +
+            "Comando v: Media dei voti\n" +
+            "Comando m: Moda dei voti\n" +
+            "Comando d: Mediana dei voti");
+            var Choice = Console.Read();
+            switch(Choice)
+            {
+                
+                case 'l':
+                case 'L':
+                    Console.WriteLine("Entrato nello switch");
+                    //foreach(var Student in StudentsList)
+                    //{
+                    //    Console.WriteLine("Entrato nel ciclo");
+                    //    Console.WriteLine(Student.ToString());
+                    //}
+                    //for(int i =0;i<StudentsList.Count;i++)
+                    //{
+                    //    Console.WriteLine("Entrato nel ciclo");
+                    //    Console.WriteLine(StudentsList[i].ToString());            
+                    //}
+                    break;
+
+
+            }
+        }
+        private void Add(Student newStudent)
+        {
+            StudentsList.Add(newStudent);
+        }
+        
+    }
+}
---
+++ StudentsSolution/StudentsSolution/StudentsSolution/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentsSolution
+{
+    class Student
+    {
+        private int ID { get; set; }
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private int Age { get; set; }
+        private string Gender { get; set; }
+        private double Vote { get; set; }
+
+        public Student(int iD, string firstName, string lastName, int age, string gender, double vote)
+        {
+            ID = iD;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Gender = gender;
+            Vote = vote;
+        }
+
+        //private Student(string ID, string FirstName, string LastName, string Age, string Gender, string Vote)
+        //{
+
+        //}
+    }
+}
